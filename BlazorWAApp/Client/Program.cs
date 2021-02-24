65f508e7e585c8d64ba29df77c08856520cb4636
Alexis Aravna <aravenapro98@gmail.com>
2021-02-24 10:46:11 -0300

    Implementacion de Syncfusion

--- BlazorWAApp/Client/Program.cs
+++ BlazorWAApp/Client/Program.cs
@@ -1,22 +1,21 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Syncfusion.Blazor;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-
-namespace BlazorWAApp.Client {
-    public class Program {
-        public static async Task Main(string[] args) {
+namespace BlazorWAApp.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddSyncfusionBlazor();
             await builder.Build().RunAsync();
         }
     }
