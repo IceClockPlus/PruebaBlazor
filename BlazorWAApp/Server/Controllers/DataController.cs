c75c87c89b47e0a34b37aec343036cae2d52e483
Alexis Aravena <alexis.aravena@gateway-dlt.cl>
2021-02-25 17:36:18 -0300

    Controller para gestionamiento de archivos

---
+++ BlazorWAApp/Server/Controllers/DataController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Syncfusion.EJ2.FileManager.Base;
+using Syncfusion.EJ2.FileManager.PhysicalFileProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWAApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class DataController : Controller
+    {
+        public PhysicalFileProvider operation;
+        public string basePath;
+
+        public object FileOperations([FromBody]FileManagerDirectoryContent args)
+        {
+            switch (args.Action)
+            {
+                case "read":
+                    return this.operation.ToCamelCase(this.operation.GetFiles(args.Path, args.ShowHiddenItems));
+                case "delete":
+                    return this.operation.ToCamelCase(this.operation.Delete(args.Path, args.Names));
+                case "copy":
+                    return this.operation.ToCamelCase(this.operation.Copy(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData));
+                case "move":
+                    return this.operation.ToCamelCase(this.operation.Move(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData));
+                case "details":
+                    return this.operation.ToCamelCase(this.operation.Details(args.Path, args.Names));
+                case "create":
+                    return this.operation.ToCamelCase(this.operation.Create(args.Path, args.Name));
+                case "search":
+                    return this.operation.ToCamelCase(this.operation.Search(args.Path,args.SearchString,args.ShowHiddenItems,args.CaseSensitive));
+                case "rename":
+                    return this.operation.ToCamelCase(this.operation.Rename(args.Path,args.Name,args.NewName));
+            }
+            return null;
+        }
+    }
+}
