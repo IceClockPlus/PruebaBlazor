b110140e6885f0643c256302f1076043ef01c736
JP Schmiede <juanpablo.schmiede@gateway-dlt.cl>
2021-02-25 15:35:36 -0300

    Licencia Syncfusion

--- BlazorWAApp/Client/Program.cs
+++ BlazorWAApp/Client/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Syncfusion.Blazor;
+using Syncfusion.Licensing;
+
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -15,6 +17,7 @@ namespace BlazorWAApp.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            SyncfusionLicenseProvider.RegisterLicense("NDA0MzY5QDMxMzgyZTM0MmUzMEQzOHhNNUh4UGxBWHRreTVIbmVybGZ2cmlXQmQ5RDF3eUYyS25DMWhXMXM9");
             builder.Services.AddSyncfusionBlazor();
             await builder.Build().RunAsync();
         }
