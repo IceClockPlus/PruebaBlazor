07b6ee29be451b7d239725e1d997f3c997228fa5
Alexis Aravna <aravenapro98@gmail.com>
2021-03-04 11:17:42 -0300

    Visualizacion PDF

--- BlazorWAApp/Client/Pages/GestorArchivo.razor.cs
+++ BlazorWAApp/Client/Pages/GestorArchivo.razor.cs
@@ -1,6 +1,18 @@
-﻿namespace BlazorWAApp.Client.Pages
+﻿using Newtonsoft.Json;
+using Syncfusion.Blazor.FileManager;
+using Syncfusion.Blazor.Spinner;
+using Syncfusion.Blazor.PdfViewer;
+
+namespace BlazorWAApp.Client.Pages
 {
     public partial class GestorArchivo
     {
+        private bool IsVisible { get; set; }
+        private string Document { get; set; }
+        SfSpinner spinner;
+        private SfPdfViewer pdfViewer;
+
+
+        
     }
 }
